675f939d824652e5c254c4ac71031e21d03f80d8
nerd19 <35899872+nerd19@users.noreply.github.com>
2018-02-24 13:37:34 -0800

    Add files via upload

---
+++ DateQuickSort.java
@@ -0,0 +1,6 @@
+public class DateQuickSort extends Date {
+    public static void main(String[] args) {
+        Date today = new Date("February", 24, 2018);
+        System.out.print(today);
+    }
+}
